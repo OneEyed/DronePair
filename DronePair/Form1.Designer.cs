0ac7cddcd05bddb17acb74f4f762247019662de2
OneEyed <joelruiz2@gmail.com>
2014-03-04 10:13:03 -0600

    Initial Commit
    AR Drone 1.0 Wifi Pairing between Drone and PC device

---
+++ DronePair/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace DronePair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Step 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Connect to Drone\'s WiFi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Step 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Click Pair Button Here:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(42, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Pair";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Note:";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(26, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(245, 51);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "If you see LED lights blink, it worked!  Only this Laptop or PC will be able to c" +
+                "onnect to the drone.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(42, 249);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Your Mac Address will appear here";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(178, 194);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 29);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Done!";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(153, 31);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Pair Drone";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(33, 160);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(162, 20);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Enter new WiFi Name";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(18, 125);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(80, 25);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Step 2:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(33, 75);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(181, 20);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Connect to Drone\'s WiFi";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(18, 41);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(80, 25);
+            this.label13.TabIndex = 10;
+            this.label13.Text = "Step 1:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(37, 191);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(240, 29);
+            this.textBox2.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(294, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 351);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rename Drone";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(37, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 33);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Update Name";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(180, 233);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 29);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Done!";
+            // 
+            // label14
+            // 
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(32, 298);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(245, 51);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "Please restart drone by removing battery and reconnecting to see new name in Wire" +
+                "less Networks.";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(17, 273);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(47, 20);
+            this.label15.TabIndex = 17;
+            this.label15.Text = "Note:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 364);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ DronePair/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Management;
+
+namespace DronePair
+{
+    public partial class Form1 : Form
+    {
+        UdpClient udp;
+        int iSequence;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            udp = null;
+            label7.Visible = false; //"Done" label starts hidden
+            label9.Visible = false;
+            iSequence = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (udp == null)
+            {
+                try
+                {
+                    udp = new UdpClient(5556); //Create our UDP Socket
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Please disconnect any controller apps that are connected to the drone.", "ERROR");
+                    return;
+                }
+            }
+            //Manually set the default Drone IP and End Point (contains port)
+            IPAddress droneAddress = IPAddress.Parse("192.168.1.1");
+            IPEndPoint droneEP = new IPEndPoint(droneAddress, 5556);
+
+            //Our encoder to convert to string to bytes
+            System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+
+            //Find the Mac Address of Network Card that is using ip 192.168.1.X
+            string szMacAddress = FindMacAddress();
+
+            //If we found a mac address, send the command
+            if (szMacAddress.Length > 0)
+            {
+                //Encode the command into a byte array
+                byte[] byteCommand = new byte[256];
+                string stringCommand = "AT*CONFIG="+(iSequence++)+",\"network:owner_mac\",\"" + szMacAddress + "\"\r";
+                byteCommand = enc.GetBytes(stringCommand);
+
+                if( udp.Client == null || !udp.Client.Connected )
+                    //Connect to Drone
+                    udp.Connect(droneEP);
+
+                //Send Command
+                udp.Send(byteCommand, stringCommand.Length);
+
+                if( udp.Client.Connected )
+                    //Disconnect from Drone.
+                    udp.Close();
+
+                udp = null;                   
+                //Show "Done" label
+                label7.Visible = true;
+            }
+            //This will only occur if there is no 192.168.1.X ip found on your system.
+            else
+            {
+                MessageBox.Show("There seems to be an error!  Make sure you are connected to the AR Drone!", "ERROR");
+            }
+        }
+
+
+
+        public string FindMacAddress()
+        {
+            //Query our Network Card information from the system
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            string szMacAddress = "";
+
+            //Loop through all Network Cards found
+            foreach (ManagementObject mo in moc)
+            {
+                //If there is no MacAddress, we can safely skip
+                if (mo["MacAddress"] == null)
+                    continue;
+
+                //Convert MacAddress to string format
+                szMacAddress = mo["MacAddress"].ToString();
+
+                //Grab the list of IP Address on Network Card
+                string[] szIPAddresses = (string[])mo["IPAddress"];
+
+                //No IP, we can safely skip
+                if (szIPAddresses == null) 
+                    continue;
+
+                //Loop through each IP in the Network Card
+                for (int i = 0; i < szIPAddresses.Length; i++)
+                {
+                    //Skip if the IP does not have matching first 3 octets of the IP
+                    if (!szIPAddresses[i].Contains("192.168.1"))
+                        continue;
+
+                    //If we got this far, the MacAddress could be a winner!
+                    szMacAddress = mo["MacAddress"].ToString();
+
+                    //If it fails here, you need to dump your Network Card
+                    if (szMacAddress.Length == 0)
+                        continue;
+
+                    //Show the Mac Address to the User
+                    textBox1.Text = szMacAddress;
+
+                    return szMacAddress;
+                }
+            }
+
+            return ""; //Didn't find anything =(
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (udp == null)
+            {
+                try
+                {
+                    udp = new UdpClient(5556); //Create our UDP Socket
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Please disconnect any controller apps that are connected to the drone.", "ERROR");
+                    return;
+                }
+            }
+            //Manually set the default Drone IP and End Point (contains port)
+            IPAddress droneAddress = IPAddress.Parse("192.168.1.1");
+            IPEndPoint droneEP = new IPEndPoint(droneAddress, 5556);
+
+            //Our encoder to convert to string to bytes
+            System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+
+            //Find the Mac Address of Network Card that is using ip 192.168.1.X
+            string szName = textBox2.Text;
+
+            //If we found a mac address, send the command
+            if (szName.Length > 0)
+            {
+                //Encode the command into a byte array
+                byte[] byteCommand = new byte[256];
+                string stringCommand = "AT*CONFIG=" + (iSequence++) + ",\"network:ssid_single_player\",\"" + szName + "\"\r";
+                byteCommand = enc.GetBytes(stringCommand);
+
+                if (udp.Client == null || !udp.Client.Connected)
+                    //Connect to Drone
+                    udp.Connect(droneEP);
+
+                //Send Command
+                udp.Send(byteCommand, stringCommand.Length);
+
+                if (udp.Client.Connected)
+                    //Disconnect from Drone.
+                    udp.Close();
+
+                udp = null;
+                //Show "Done" label
+                label9.Visible = true;
+            }
+            //This will only occur if there is no 192.168.1.X ip found on your system.
+            else
+            {
+                MessageBox.Show("There seems to be an error!  Make sure you are connected to the AR Drone!", "ERROR");
+            }
+        }
+    }
+}
